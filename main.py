c47cb59da83d112cc186ff7ab302475f7b058eaf
melaniseg <melanisegadsden@gmail.com>
2026-02-20 00:14:59 -0500

    Added user prompt and messages component to main function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 
 def main():
@@ -10,9 +11,12 @@ def main():
     if not api_key:
        raise RuntimeError("API Key cannot be found")
     client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(model='gemini-2.5-flash', 
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
-    #tokendata = genai.types.GenerateContentResponse.usage_metadata
+    contents=messages)
     if response.usage_metadata is None:
        raise RuntimeError("Failed API request")
     else:
