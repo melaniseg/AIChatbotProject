54cf4e7d6d11f0c4e914fe0d4acfcdf3737b060f
melaniseg <melanisegadsden@gmail.com>
2026-02-22 12:45:38 -0500

    Adds verbose output to main function

--- main.py
+++ main.py
@@ -13,15 +13,17 @@ def main():
     client = genai.Client(api_key=api_key)
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(model='gemini-2.5-flash', 
     contents=messages)
     if response.usage_metadata is None:
        raise RuntimeError("Failed API request")
-    else:
+    if args.verbose:
        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+       print(f"User prompt: {args.user_prompt}")
     print(response.text)
 
 if __name__ == "__main__":
